35912c24fff8d7054b455a89449fc07c41c0a597
Damir <damir9715@gmail.com>
2020-09-13 12:12:43 +0600

    data binding

--- app/src/main/java/com/example/todolist/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/todolist/overview/OverviewFragment.kt
@@ -44,7 +44,7 @@ class OverviewFragment : Fragment() {
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
