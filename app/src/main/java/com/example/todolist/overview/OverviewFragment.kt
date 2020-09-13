0d20d1cdfbd8cb29c51b07c60c7470bee2e3db3b
Damir <damir9715@gmail.com>
2020-09-13 13:36:51 +0600

    view click listener

--- app/src/main/java/com/example/todolist/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/todolist/overview/OverviewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -43,7 +44,9 @@ class OverviewFragment : Fragment() {
         val manager = GridLayoutManager(application, 2)
         binding.tasksList.layoutManager = manager
 
-        val adapter = TaskAdapter()
+        val adapter = TaskAdapter(TaskListener { taskId ->
+            Toast.makeText(context, "$taskId", Toast.LENGTH_SHORT).show()
+        })
         binding.tasksList.adapter = adapter
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
--- app/src/main/java/com/example/todolist/overview/TaskAdapter.kt
+++ app/src/main/java/com/example/todolist/overview/TaskAdapter.kt
@@ -8,11 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.database.Task
 import com.example.todolist.databinding.TextItemTaskBinding
 
-class TaskAdapter : ListAdapter<Task, TaskAdapter.ViewHolder>(TaskDiffCallback()) {
+class TaskAdapter(val clickListener: TaskListener) :
+    ListAdapter<Task, TaskAdapter.ViewHolder>(TaskDiffCallback()) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
-        holder.bind(item)
+        holder.bind(item, clickListener)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,9 +22,12 @@ class TaskAdapter : ListAdapter<Task, TaskAdapter.ViewHolder>(TaskDiffCallback()
 
     class ViewHolder private constructor(val binding: TextItemTaskBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: Task) {
+        fun bind(item: Task, clickListener: TaskListener) {
+            binding.task = item
             binding.taskTitle.text = item.title
             binding.taskDescription.text = item.description
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
         }
 
         companion object {
@@ -44,4 +48,8 @@ class TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
     override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
         return oldItem == newItem
     }
+}
+
+class TaskListener(val clickListener: (taskId: Long) -> Unit) {
+    fun onClick(task: Task) = clickListener(task.taskId)
 }
