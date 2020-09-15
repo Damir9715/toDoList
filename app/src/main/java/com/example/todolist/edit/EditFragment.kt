6175172afdbba192d128ae9223278311c16182cb
Damir <damir9715@gmail.com>
2020-09-15 18:40:29 +0600

    save task onStop

--- app/src/main/java/com/example/todolist/edit/EditFragment.kt
+++ app/src/main/java/com/example/todolist/edit/EditFragment.kt
@@ -16,44 +16,55 @@ import com.example.todolist.databinding.FragmentEditBinding
 
 class EditFragment : Fragment() {
     private lateinit var binding: FragmentEditBinding
+    private lateinit var task: Task
+    private lateinit var viewModel: EditViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val task = EditFragmentArgs.fromBundle(requireArguments()).task
+        task = EditFragmentArgs.fromBundle(requireArguments()).task
 
         val application = requireNotNull(this.activity).application
         val dao = ToDoListDatabase.getInstance(application).toDoListDatabaseDao
         val viewModelFactory = EditViewModelFactory(task, dao)
-        val viewModel = ViewModelProvider(this, viewModelFactory).get(EditViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(EditViewModel::class.java)
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
         binding.saveFab.setOnClickListener {
-            //if new task
-            if (viewModel.selectedTask.value!!.taskId == -1L) {
-                viewModel.saveTask(
-                    Task(
-                        title = binding.title.text.toString(),
-                        description = binding.taskDescription.text.toString()
-                    )
-                )
-            } else {
-                viewModel.updateTask(
-                    Task(
-                        taskId = task.taskId,
-                        title = binding.title.text.toString(),
-                        description = binding.taskDescription.text.toString()
-                    )
-                )
-            }
+            saveButton(viewModel)
             Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show()
         }
 
         return binding.root
+    }
+
+    override fun onStop() {
+        super.onStop()
+        saveButton(viewModel)
+    }
+
+    private fun saveButton(viewModel: EditViewModel) {
+        //if new task
+        if (viewModel.selectedTask.value!!.taskId == -1L) {
+            viewModel.saveTask(
+                Task(
+                    title = binding.title.text.toString(),
+                    description = binding.taskDescription.text.toString()
+                )
+            )
+        } else {
+            viewModel.updateTask(
+                Task(
+                    taskId = viewModel.selectedTask.value!!.taskId,
+                    title = binding.title.text.toString(),
+                    description = binding.taskDescription.text.toString()
+                )
+            )
+        }
     }
 }
