881c61bc832e2926e1d8201022c2d187857e2059
Damir <damir9715@gmail.com>
2020-09-21 17:56:03 +0600

    repository layer

--- app/src/main/java/com/example/todolist/edit/EditViewModelFactory.kt
+++ app/src/main/java/com/example/todolist/edit/EditViewModelFactory.kt
@@ -1,16 +1,15 @@
 package com.example.todolist.edit
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.todolist.database.Task
-import com.example.todolist.database.ToDoListDatabaseDao
 
 @Suppress("UNCHECKED_CAST")
-class EditViewModelFactory(
-    private val task: Task, private val dao: ToDoListDatabaseDao) : ViewModelProvider.Factory {
+class EditViewModelFactory(private val task: Task, val app: Application) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(EditViewModel::class.java)) {
-            return EditViewModel(task, dao) as T
+            return EditViewModel(task, app) as T
         }
         throw IllegalArgumentException("Unknown VM class")
     }
