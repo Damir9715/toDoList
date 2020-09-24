85cc2cbd4d266b5417d22672907cd6c05e8e57b4
Damir <damir9715@gmail.com>
2020-09-24 18:59:15 +0600

    selection mode

--- app/src/main/java/com/example/todolist/overview/TaskAdapter.kt
+++ app/src/main/java/com/example/todolist/overview/TaskAdapter.kt
@@ -1,21 +1,62 @@
 package com.example.todolist.overview
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.database.Task
 import com.example.todolist.databinding.GridItemBinding
+import kotlinx.android.synthetic.main.grid_item.view.*
 
 class TaskAdapter(val clickListener: TaskListener) :
     ListAdapter<Task, TaskAdapter.ViewHolder>(TaskDiffCallback()) {
 
+    private var isSelectMode = false
+    private val selectedItems = mutableListOf<Task>()
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
+
         holder.itemView.setOnClickListener {
-            clickListener.onClick(item)
+            if (isSelectMode) {
+                if (item in selectedItems) {
+                    holder.itemView.checkBox.isChecked = false
+                    selectedItems.remove(item)
+                } else {
+                    holder.itemView.checkBox.isChecked = true
+                    selectedItems.add(item)
+                }
+            } else
+                clickListener.onClick(item)
+            println("selectedItem: ${selectedItems.map { it.taskId }}")
         }
+
+        holder.itemView.setOnLongClickListener {
+            if (isSelectMode) {
+                isSelectMode = false
+                holder.binding.checkBox.visibility = View.GONE
+                holder.itemView.checkBox.isChecked = false  //todo doesn't uncheck all
+                selectedItems.clear()
+            } else {
+                isSelectMode = true
+                holder.binding.checkBox.visibility = View.VISIBLE
+                selectedItems.add(item)
+            }
+            this.notifyDataSetChanged()
+            true
+        }
+
+        if (isSelectMode) {
+            holder.itemView.checkBox.visibility = View.VISIBLE
+        } else {
+            holder.itemView.checkBox.visibility = View.GONE
+        }
+
+        if (item in selectedItems)
+            holder.itemView.checkBox.isChecked = true
+
         holder.bind(item)
     }
 
@@ -53,7 +94,6 @@ class TaskAdapter(val clickListener: TaskListener) :
         }
     }
 }
-
 
 class TaskListener(val clickListener: (task: Task) -> Unit) {
     fun onClick(task: Task) = clickListener(task)
