e43edc799fab1a288a739159b6bfa4934c2c0a7c
Damir <damir9715@gmail.com>
2020-09-25 18:33:10 +0600

    fix overviewVM factory

--- app/src/main/java/com/example/todolist/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/todolist/overview/OverviewFragment.kt
@@ -16,6 +16,7 @@ import com.example.todolist.database.Task
 import com.example.todolist.database.TaskStatus
 import com.example.todolist.database.ToDoListDatabase
 import com.example.todolist.databinding.FragmentOverviewBinding
+import com.example.todolist.repository.TaskRepository
 
 
 class OverviewFragment : Fragment() {
@@ -39,8 +40,8 @@ class OverviewFragment : Fragment() {
 
         fragmentActivity = requireActivity()
         val application = fragmentActivity.application
-        val dao = ToDoListDatabase.getInstance(application).dao
-        val viewModelFactory = OverviewViewModelFactory(dao)
+        val db = ToDoListDatabase.getInstance(application)
+        val viewModelFactory = OverviewViewModelFactory(TaskRepository(db))
         viewModel = ViewModelProvider(this, viewModelFactory).get(OverviewViewModel::class.java)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
