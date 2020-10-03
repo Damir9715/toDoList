cf793c6e0bb5ba18874273d4972cc9db8cd00ac8
Damir <damir9715@gmail.com>
2020-10-03 22:55:53 +0600

    dao unit test

--- app/src/androidTest/java/com/example/todolist/database/ToDoListDatabaseTest.kt
+++ app/src/androidTest/java/com/example/todolist/database/ToDoListDatabaseTest.kt
@@ -6,6 +6,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.example.todolist.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
@@ -48,5 +49,55 @@ class ToDoListDatabaseTest {
         val byTitle = dao.get(task.taskId)
 
         assertThat(byTitle.title).isEqualTo(task.title)
+    }
+
+    @Test
+    fun updateTask() = runBlockingTest {
+        val task = Task(taskId = 1, title = "title")
+        dao.insert(task)
+        dao.insert(task.copy(title = "newTitle"))
+        val byTitle = dao.get(task.taskId)
+
+        assertThat(byTitle.title).isEqualTo("newTitle")
+    }
+
+    @Test
+    fun getAllTasksByStatus() = runBlockingTest {
+        val task1 = Task(taskId = 1, title = "titleOne", status = 0)
+        val task2 = Task(taskId = 2, title = "titleTwo", status = 1)
+        dao.insert(task1)
+        dao.insert(task2)
+
+        val byStatus = dao.getAllTasksByStatus(task1.status.toString()).getOrAwaitValue()
+
+        assertThat(byStatus).contains(task1)
+        assertThat(byStatus).doesNotContain(task2)
+    }
+
+    @Test
+    fun deleteTask() = runBlockingTest {
+        val task = Task(taskId = 1, title = "title")
+        dao.insert(task)
+        dao.delete(task)
+        val byTitle = dao.get(task.taskId)
+
+        assertThat(byTitle).isNull()
+    }
+
+    @Test
+    fun deleteListOfTasks() = runBlockingTest {
+        val listOfTasks = listOf(
+            Task(taskId = 1, title = "titleOne"),
+            Task(taskId = 2, title = "titleTwo"),
+            Task(taskId = 3, title = "titleThree")
+        )
+        dao.insert(listOfTasks[0])
+        dao.insert(listOfTasks[1])
+        dao.insert(listOfTasks[2])
+        dao.deleteList(listOfTasks.map { it.taskId })
+
+        val byId = dao.get(listOfTasks[0].taskId)
+
+        assertThat(byId).isNull()
     }
 }
