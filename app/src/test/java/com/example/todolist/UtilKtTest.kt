f12d08c5a6734f5b954266044afb7c86ac5fdbf6
Damir <damir9715@gmail.com>
2020-10-03 21:06:43 +0600

    util unit test

---
+++ app/src/test/java/com/example/todolist/UtilKtTest.kt
@@ -0,0 +1,39 @@
+package com.example.todolist
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class UtilKtTest {
+
+    @Test
+    fun truncateOnHalfOfWord() {
+        val subject = "Hello world"
+        val result = subject.smartTruncate(2)
+
+        assertThat(result).isEqualTo("Hello...")
+    }
+
+    @Test
+    fun truncateOnSpacePunctuation() {
+        val subject = "Hello world"
+        val result = subject.smartTruncate(5)
+
+        assertThat(result).isEqualTo("Hello...")
+    }
+
+    @Test
+    fun truncateOnOtherPunctuation() {
+        val subject = "Hello, world"
+        val result = subject.smartTruncate(6)
+
+        assertThat(result).isEqualTo("Hello...")
+    }
+
+    @Test
+    fun truncateAll() {
+        val subject = "Hello world"
+        val result = subject.smartTruncate(11)
+
+        assertThat(result).isEqualTo("Hello world")
+    }
+}
